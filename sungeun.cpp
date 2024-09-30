167b0b84fb8a61cdb81ec36952584a2564eca87e
Sparrow Coding <163827077+SparrowCoding@users.noreply.github.com>
2024-09-30 19:05:03 +0900

    Update sungeun.cpp

--- sungeun.cpp
+++ sungeun.cpp
@@ -2,12 +2,8 @@
 
 using namespace std;
 int main() {
-    int x, y;
-    cin >> x >> y;
-    cout << x + y << endl;
-    cout << x - y << endl;
-    cout << x * y << endl;
-    cout << x % y << endl;
+
+    cout << "Hello World" << endl;
 
 
     return 0;
